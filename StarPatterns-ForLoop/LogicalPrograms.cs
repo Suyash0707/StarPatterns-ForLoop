a34e0db1c9507e9fa7ab9656f85915b9b7cd933a
Suyash0707 <suyashb175@gmail.com>
2023-03-20 13:38:34 +0530

    Added Logical Programs

---
+++ StarPatterns-ForLoop/LogicalPrograms.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatterns_ForLoop
+{
+    internal class LogicalPrograms
+    {
+        public void OddNumbers()
+        {
+            Console.WriteLine("Please enter the limit for odd numbers");
+            int Limit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Odd Numbers Are:");
+
+            for (int i = 1; i <= Limit; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine();
+                
+            }
+        }
+
+        public void Tables()
+        {
+            Console.WriteLine("Please Enter the limit for Tables");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Displaying Tables:");
+
+            for(int i = 1; i <= limit; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write("|" + j * i);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- StarPatterns-ForLoop/Program.cs
+++ StarPatterns-ForLoop/Program.cs
@@ -4,16 +4,21 @@ public class program
 {
     public static void Main()
     {
-        Patterns obj = new Patterns();
+        //Patterns obj = new Patterns();
 
-        obj.pattern1();
-        obj.pattern2();
-        obj.pattern3();
-        obj.pattern4();
-        obj.pattern5();
-        obj.pattern6();
-        obj.pattern7();
-        obj.pattern8();
+        //obj.pattern1();
+        //obj.pattern2();
+        //obj.pattern3();
+        //obj.pattern4();
+        //obj.pattern5();
+        //obj.pattern6();
+        //obj.pattern7();
+        //obj.pattern8();
+
+        LogicalPrograms obj2 = new LogicalPrograms();
+        obj2.OddNumbers();
+        obj2.Tables();
+        
             
     }
 }
